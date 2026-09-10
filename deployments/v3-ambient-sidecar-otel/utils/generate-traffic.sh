a73a006b2a9158ab9a8108c18c9dc45445e36c07
ralopez <ralopez@redhat.com>
2026-09-10 18:06:37 +0200

    Despliegue de istio con sidecar + ambient + open telemetry

---
+++ deployments/v3-ambient-sidecar-otel/utils/generate-traffic.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Genera tráfico en el mesh llamando a los endpoints:
+#   - /productpage
+#   - /api/v1/products
+HOSTNAME=bookinfo.rabadillo.tia.com
+
+echo -e "Usando hostname: $HOSTNAME\n"
+
+# Comprobar que se ha pasado un argumento
+if [ $# -ne 1 ]; then
+  echo "Uso: $0 <numero_de_iteraciones>"
+  exit 1
+fi
+
+ITERACIONES=$1
+
+# Validar que es un número entero positivo
+if ! [[ "$ITERACIONES" =~ ^[0-9]+$ ]]; then
+  echo "Error: el parámetro debe ser un número entero positivo."
+  exit 1
+fi
+
+# Validar que es mayor que 0
+if [ "$ITERACIONES" -le 0 ]; then
+  echo "Error: el número debe ser mayor que 0."
+  exit 1
+fi
+
+# Bucle
+for ((i=1; i<=ITERACIONES; i++)); do
+  echo -n "Iteración $i... "
+#  curl -s http://$HOSTNAME/productpage > /dev/null
+  http_code_page=$(curl -sk -w "%{http_code}" -o /dev/null http://$HOSTNAME/sidecar/productpage)
+  sleep 0.5
+  http_code_json=$(curl -sk -w "%{http_code}" -o /dev/null http://$HOSTNAME/sidecar/api/v1/products)
+  echo -e "(${http_code_page}/${http_code_json})"
+
+  http_code_page=$(curl -sk -w "%{http_code}" -o /dev/null http://$HOSTNAME/ambient/productpage)
+  sleep 0.5
+  http_code_json=$(curl -sk -w "%{http_code}" -o /dev/null http://$HOSTNAME/ambient/api/v1/products)
+  echo -e "               (${http_code_page}/${http_code_json})"
+
+done
+
+echo "Ejecución completada."
